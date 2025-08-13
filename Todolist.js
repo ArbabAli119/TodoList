3c152df00457edca2c112eb57a571ae24210ccb0
Arbab Ali <93856092+ArbabAli119@users.noreply.github.com>
2025-08-13 12:12:57 +0530

    Update Todolist.js

--- Todolist.js
+++ Todolist.js
@@ -1,23 +1,41 @@
-
-//In This Todo Only delete 
 let btn = document.querySelector("button");
 let input = document.querySelector("input");
 let ul = document.querySelector("ul");
 
 btn.addEventListener("click", function() {
-    let item = document.createElement("li");
-    item.innerText = input.value;
 
-    let delBtn = document.createElement("button");
-    delBtn.innerText = "Delete";
-    delBtn.classList.add("delete");
-    item.appendChild(delBtn);
+    if(input.value == ''){
+        alert('enter any task')
 
-    ul.appendChild(item);  
-    input.value = "";
+    }
+    else {
+    
+        let item = document.createElement("li");
+        let span = document.createElement("span");
+        span.innerText = input.value;
+        let checkbox = document.createElement("input");
+        checkbox.type = "checkbox";
+
+        checkbox.addEventListener("change", function() {
+        if (this.checked) {
+            span.style.textDecoration = "line-through";
+        } else {
+            span.style.textDecoration = "none";
+        }
+     });
+     
+        item.appendChild(checkbox);     
+         item.appendChild(span);
+         ul.appendChild(item);  
+        input.value = "";
+  
+        let delBtn = document.createElement("button");
+        delBtn.innerText = "Delete";
+        delBtn.classList.add("delete");
+        item.appendChild(delBtn);        
+    }
 });
 
-// Using event delegation to handle delete button clicks
 ul.addEventListener("click", function(event) {
     if(event.target.nodeName =="BUTTON") {
         let listItem = event.target.parentElement;
@@ -25,56 +43,3 @@ ul.addEventListener("click", function(event) {
         console.log("Deleted")
     }
 });
-
-
-
-//In This Todos checkbox and Delete  work
-
-
-// let btn = document.querySelector("button");
-// let input = document.querySelector("input");
-// let ul = document.querySelector("ul");
-
-// btn.addEventListener("click", function() {
-//     let item = document.createElement("li");
-
-//     // Create checkbox
-//     let checkbox = document.createElement("input");
-//     checkbox.type = "checkbox";
-
-//     // Create span to hold todo text
-//     let span = document.createElement("span");
-//     span.innerText = input.value;
-
-//     // When checkbox changes, toggle strikethrough on span
-//     checkbox.addEventListener("change", function() {
-//         if (this.checked) {
-//             span.style.textDecoration = "line-through";
-//         } else {
-//             span.style.textDecoration = "none";
-//         }
-//     });
-
-//     // Create delete button
-//     let delBtn = document.createElement("button");
-//     delBtn.innerText = "Delete";
-//     delBtn.classList.add("delete");
-
-//     // Append checkbox, span, and delete button to li
-//     item.appendChild(checkbox);
-//     item.appendChild(span);
-//     item.appendChild(delBtn);
-
-//     ul.appendChild(item);  
-//     input.value = "";
-// });
-
-// // Using event delegation to handle delete button clicks
-// ul.addEventListener("click", function(event) {
-//     if(event.target.nodeName == "BUTTON") {
-//         let listItem = event.target.parentElement;
-//         listItem.remove();
-//         console.log("Deleted");
-//     }
-// });
-
